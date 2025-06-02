91b17e8d98c7cb94ff8ec1ad78191741ecb0e46d
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 13:04:56 +0530

    Refactor Index component: replace buttons and image with navigation links to profile and notifications for improved user experience.

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,14 @@
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
+import { View } from "react-native";
 import { styles } from "../styles/auth.styles";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <TouchableOpacity style={styles.button} onPress={() => alert("clicked")}>
-        <Text style={styles.buttonText}>Click me</Text>
-      </TouchableOpacity>
-      <Image source={require("../assets/images/icon.png")} style={styles.logo} />
 
-      <Pressable style={styles.button} onPress={() => alert("clicked")}>
-        <Text style={styles.buttonText}>Click me</Text>
-      </Pressable>
+      <Link href="/profile">Go to profile</Link>
+      <Link href="/notifications">Go to notifications</Link>
+     
     </View>
   );
 }
