21184bdad9b8a3cce7ea42c62e27bd6309314b0d
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 12:55:17 +0530

    Refactor Index component: update greeting text and import styles from auth.styles. Removed inline styles for cleaner code.

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text, View } from "react-native";
+import { styles } from "../styles/auth.styles";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi there</Text>
+      <Text style={styles.text}>Hello</Text>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 44,
-    color: "blue",
-    fontWeight: "bold",
-  },
-});
---
+++ styles/auth.styles.js
@@ -0,0 +1,14 @@
+const { StyleSheet } = require("react-native");
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    text: {
+      fontSize: 64,
+      color: "red",
+      fontWeight: "thin",
+    },
+  });
