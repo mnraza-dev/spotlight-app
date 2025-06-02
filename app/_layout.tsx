c01ab554c4307056e7532ada9245ba26f3c5e1e2
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 13:10:45 +0530

    Enhance RootLayout: wrap Stack component with SafeAreaProvider and SafeAreaView for improved layout handling across devices. Add screens for Feed, Profile, and Notifications with appropriate options.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} >
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "Feed" }} />
+          <Stack.Screen name="profile" options={{ title: "Profile", headerShown: true }} />
+          <Stack.Screen name="notifications" options={{ title: "Notifications", headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
