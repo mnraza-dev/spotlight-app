623bf7977523286eb2276e7a3e7fd7f0579d8d04
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 12:58:22 +0530

    Add button components to Index: implement TouchableOpacity and Pressable for user interaction; enhance styles for buttons in auth.styles.js

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../styles/auth.styles";
+
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello</Text>
+      <TouchableOpacity style={styles.button} onPress={() => alert("clicked")}>
+        <Text style={styles.buttonText}>Click me</Text>
+      </TouchableOpacity>
+
+      <Pressable style={styles.button} onPress={() => alert("clicked")}>
+        <Text style={styles.buttonText}>Click me</Text>
+      </Pressable>
     </View>
   );
 }
--- styles/auth.styles.js
+++ styles/auth.styles.js
@@ -11,4 +11,16 @@ export const styles = StyleSheet.create({
       color: "red",
       fontWeight: "thin",
     },
+    button: {
+      backgroundColor: "blue",
+      paddingHorizontal: 30,
+      paddingVertical: 15,
+      borderRadius: 15,
+      marginTop: 20,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 16,
+      fontWeight: "bold",
+    },
   });
