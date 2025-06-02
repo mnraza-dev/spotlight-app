b9081c7f7d98e27d30172e0d925fef765e1cd6f0
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 13:40:18 +0530

    Refactor TabsLayout: improve code readability by restructuring tab screen definitions for better organization and maintainability.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,24 +16,32 @@ export default function TabsLayout() {
                 borderTopColor: COLORS.surfaceLight,
             },
         }}>
-            <Tabs.Screen name="index" options={{
-                tabBarIcon: ({ size, color }) => <Ionicons name="home" size={size} color={color} />,
-            }} />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name="home" size={size} color={color} />,
+                }} />
 
-            <Tabs.Screen name="bookmarks" options={{
-                tabBarIcon: ({ size, color }) => <Ionicons name="bookmark" size={size} color={color} />,
-            }} />
-            <Tabs.Screen name="create" options={{
-                tabBarIcon: ({ size, color }) => <Ionicons name="add-circle" size={size} color={COLORS.primary} />,
-            }} />
-            <Tabs.Screen name="notifications" options={{
-                tabBarIcon: ({ size, color }) => <Ionicons name="heart" size={size} color={color} />,
-            }} />
-            <Tabs.Screen name="profile" options={{
-                tabBarIcon: ({ size, color }) => <Ionicons name="person-circle" size={size} color={color} />,
-            }} />
-
-
+            <Tabs.Screen
+                name="bookmarks"
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name="bookmark" size={size} color={color} />,
+                }} />
+            <Tabs.Screen
+                name="create"
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name="add-circle" size={size} color={COLORS.primary} />,
+                }} />
+            <Tabs.Screen
+                name="notifications"
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name="heart" size={size} color={color} />,
+                }} />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name="person-circle" size={size} color={color} />,
+                }} />
         </Tabs>
     )
 }
